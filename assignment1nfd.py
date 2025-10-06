dcf7e7032c34a808af00c8ca2c8f172cd3ce5ef5
nicoumaine <antonia.faragodumsch@maine.edu>
2025-10-06 09:54:13 -0400

    Webdriver block

--- assignment1nfd.py
+++ assignment1nfd.py
@@ -6,3 +6,12 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
+options = webdriver.ChromeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--incognito')
+# options.add_argument('--headless')  # uncomment for silent run
+
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install()),
+    options=options
+)
