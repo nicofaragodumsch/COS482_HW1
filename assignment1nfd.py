a771dedd2e0e059f1ddf943322dd8d491a3fdc8e
nicoumaine <antonia.faragodumsch@maine.edu>
2025-10-06 09:52:02 -0400

    Update import block

--- assignment1nfd.py
+++ assignment1nfd.py
@@ -1,6 +1,8 @@
+import time
+import pandas as pd
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
-from bs4 import BeautifulSoup
-import time
